06534351a35cd4ec7f20b66af695803397d70104
fairysaint <73265379+fairysaint@users.noreply.github.com>
2026-07-29 15:42:13 +0700

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const SUPABASE_URL = "https://twraggzfzojuwrwrvhta.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3cmFnZ3pmem9qdXdyd3J2aHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NTk4MzcsImV4cCI6MjA5NTQzNTgzN30.5I7eNPiF2pLCaOVZICU0KYGYpfitm5NuciC8EQ24ND8"; 
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 const { createApp } = Vue;
 
